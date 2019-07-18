d281af57b309157fff3f598e6ae77b59acbe9ce2
Volker Wieban <thesquidpeople@gmail.com>
2019-07-18 15:07:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -48,6 +48,9 @@ done
 echo 'Installing global ruby gems...'
 sudo gem install lolcat
 
+echo 'Installing global NPM modules'
+npm install --global fkill-cli
+
 echo 'Installing cheat.sh cli client...'
 curl https://cht.sh/:cht.sh | tee /usr/local/bin/cht.sh
 chmod a+x /usr/local/bin/cht.sh
